aa33fca1cc545bc8b82b8540a890beb35a7de3b3
Louis Tsai <louis993546@gmail.com>
2021-02-27 21:43:41 +0100

    Not working drag and drop; Working min/max/normal

--- src/main/kotlin/components/Window95.kt
+++ src/main/kotlin/components/Window95.kt
@@ -1,7 +1,12 @@
 package components
 
 import Color95
+import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.draggable
+import androidx.compose.foundation.gestures.rememberDraggableState
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
@@ -10,6 +15,7 @@ import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.composed
 import androidx.compose.ui.geometry.Offset
@@ -17,13 +23,18 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.LinearGradient
 import androidx.compose.ui.graphics.TileMode
+import androidx.compose.ui.input.pointer.consumeAllChanges
+import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import kotlin.math.roundToInt
 
 @Composable
 fun Window95(
     modifier: Modifier = Modifier,
+    offsetX: MutableState<Float>,
+    offsetY: MutableState<Float>,
     headerContent: @Composable RowScope.() -> Unit,
     content: @Composable () -> Unit
 ) {
@@ -34,7 +45,18 @@ fun Window95(
             .border95(Elevation.Above)
     ) {
         val borderCompensationPadding = 4.dp
-        WindowsHeader(modifier = Modifier.padding(4.dp)) {
+        val appWindow = LocalAppWindow.current
+        WindowsHeader(
+            modifier = Modifier.padding(4.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { change, dragAmount ->
+                        change.consumeAllChanges()
+                        offsetX.value += dragAmount.x
+                        offsetY.value += dragAmount.y
+
+                    }
+                },
+        ) {
             headerContent()
         }
         Box(modifier = Modifier.padding(borderCompensationPadding)) {
@@ -74,11 +96,15 @@ fun WindowsHeader(
 fun Window95(
     modifier: Modifier = Modifier,
     headerTitle: String,
+    offsetX: MutableState<Float>,
+    offsetY: MutableState<Float>,
     action: (Window95Action) -> Unit,
     content: @Composable() () -> Unit
 ) {
     Window95(
         modifier = modifier,
+        offsetX = offsetX,
+        offsetY = offsetY,
         headerContent = {
             Text(
                 modifier = Modifier.weight(1f).padding(4.dp),
--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Text
 import androidx.compose.runtime.getValue
@@ -13,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.IntOffset
 import androidx.compose.ui.unit.dp
 import components.AppBar95
 import components.Button95
@@ -21,23 +23,41 @@ import components.Cutout95
 import components.Window95
 import components.Window95Action
 import javax.imageio.ImageIO
+import kotlin.math.roundToInt
 
 fun main() {
     val title = "Compose95 Demo"
     Window(
         undecorated = true,
         title = title,
-        icon = ImageIO.read(Thread.currentThread().contextClassLoader.getResource("icon.png").openStream())
+        icon = ImageIO.read(Thread.currentThread().contextClassLoader.getResource("icon.png").openStream()),
     ) {
         val appWindow = LocalAppWindow.current
+
+        val offsetX = remember { mutableStateOf(0f) }
+        val offsetY = remember { mutableStateOf(0f) }
+
+        val previousLocationX by remember { mutableStateOf(appWindow.x) }
+        val previousLocationY by remember { mutableStateOf(appWindow.y) }
+        val previousSizeX by remember { mutableStateOf(appWindow.width) }
+        val previousSizeY by remember { mutableStateOf(appWindow.height) }
+
+        // TODO why is this making the whole window jumping around?
+//        appWindow.setLocation(offsetX.value.roundToInt(), offsetY.value.roundToInt())
 
         Window95(
             modifier = Modifier.fillMaxHeight(),
             headerTitle = title,
+            offsetX = offsetX,
+            offsetY = offsetY,
             action = {
                 when (it) {
-                    Window95Action.MinimizeClicked -> TODO("It is OS dependent, right?")
-                    Window95Action.MaximizeClicked -> TODO("how to get screen size?")
+                    Window95Action.MinimizeClicked -> appWindow.minimize()
+                    Window95Action.MaximizeClicked ->
+                        if (appWindow.isMaximized) {
+                            appWindow.setSize(previousSizeX, previousSizeY)
+                            appWindow.setLocation(previousLocationX, previousLocationY)
+                        } else appWindow.maximize()
                     Window95Action.CloseClicked -> appWindow.close()
                 }
             }

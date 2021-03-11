334ef4048eafdd036a743bf0967ccd6f276c9b06
Louis Tsai <louis993546@gmail.com>
2021-03-11 21:30:56 +0100

    Start building calculator

--- calculator/src/main/kotlin/com/louis993546/compose95/calculator/Calculator.kt
+++ calculator/src/main/kotlin/com/louis993546/compose95/calculator/Calculator.kt
@@ -2,24 +2,18 @@ package com.louis993546.compose95.calculator
 
 import androidx.compose.desktop.LocalAppWindow
 import androidx.compose.desktop.Window
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.Text
+import androidx.compose.foundation.text.BasicText
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import com.louis993546.compose95.components.AppBar95
 import com.louis993546.compose95.components.Button95
-import com.louis993546.compose95.components.Checkbox95
-import com.louis993546.compose95.components.Cutout95
 import com.louis993546.compose95.components.Window95
 import javax.imageio.ImageIO
 import com.louis993546.compose95.components.Window95Action as Action
@@ -61,19 +55,65 @@ fun main() {
                 }
             }
         ) {
-            var isChecked by remember { mutableStateOf(false) }
-            Column {
-                Button95(onClick = { }) { Text("Hello, World") }
-                Cutout95 {
-                    Checkbox95(
-                        modifier = Modifier.padding(16.dp),
-                        isCheck = isChecked,
-                        onClick = { isChecked = it }
-                    )
-                }
-                Box(Modifier.background(Color.White).fillMaxWidth()) {
-                    AppBar95()
-                }
+//            var isChecked by remember { mutableStateOf(false) }
+//            Column {
+//                Button95(onClick = { }) { Text("Hello, World") }
+//                Cutout95 {
+//                    Checkbox95(
+//                        modifier = Modifier.padding(16.dp),
+//                        isCheck = isChecked,
+//                        onClick = { isChecked = it }
+//                    )
+//                }
+//                Box(Modifier.background(Color.White).fillMaxWidth()) {
+//                    AppBar95()
+//                }
+//            }
+            Body95()
+        }
+    }
+}
+
+@Composable
+fun Body95(
+    modifier: Modifier = Modifier,
+) {
+    // menu row
+    // divider
+    Calculator95()
+}
+
+@Composable
+fun Calculator95(
+    modifier: Modifier = Modifier,
+) {
+    val value by remember { mutableStateOf(0.0) }
+    val isScientific by remember { mutableStateOf(false) }
+
+    ColumnOfButtons95(
+        keys = listOf("7", "4", "1", "0"),
+        onKeyClick = { println("$it clicked") },
+    )
+}
+
+data class Key(
+    val label: String,
+    val color: Color, // TODO
+)
+
+@Composable
+fun ColumnOfButtons95(
+    modifier: Modifier = Modifier,
+    keys: List<String>,
+    onKeyClick: (String) -> Unit,
+) {
+    Column(modifier = modifier) {
+        keys.forEach { key ->
+            Button95(onClick = { onKeyClick(key) }) {
+                BasicText(
+                    modifier = Modifier.padding(horizontal = 8.dp, vertical = 4.dp),
+                    text = key,
+                )
             }
         }
     }
